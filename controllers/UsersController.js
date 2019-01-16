c60d9fd143b52d8e7eadbf8ce96dc7daeba04a09
Rahmat Wahyu Hadi <hrahmatwahyu@gmail.com>
2019-01-16 23:05:35 +0700

    login with JWT

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -23,41 +23,59 @@ exports.getAllUsers = async (req, res, next) => {
 	})
 }
 
-
+/* Login Users */
 exports.loginUser = async (req, res, next) => {
 	
 	/* get Email When true in DB  */
 	function getEmail (callback){
-
+		/* validasi untuk body yang dibutuhkan */
 		if (!req.body.email || !req.body.password) {			
 			return res.status(401).json({ code: 401, error: true,status: 'error', message: "Email and Password Cannot be Empety", result: null});
 		}
 
-		let isParams = { where : { email : req.body.email} }
+		let isParams = {  /* for query get emails */
+			where: { email: req.body.email }
+		}
+
 		QueryService.SelectOne('users', isParams, (err, result) => {
 			if (!result || err) {
 				return res.status(401).json({ code: 401, error: true,status: 'error', message: "Opps your user not found", result: null});
 			} else {
-				callback(null , req.body.password, result)
+				if (result.role !== 'user') {
+					return res.status(401).json({ code: 401, error: true, status: 'error', message: "Opps only user can login", result: null});
+				} else {
+					callback(null , req.body.password, result)
+				}
+				
 			}
 		});
 		
 	}
 
 	/* for compare password with bcrypt */
-	function getDefaultPassword (password, args , callback) {
-		callback(null, password)
+	function checkValidPassword (isPassword, isDataGetMail , callback) {
+		if (!isDataGetMail.validPassword(isPassword)) {
+			return res.status(401).json({ code: 401, error: true,status: 'error', message: "Wrong Password", result: null});
+		} else {
+			let userLogin = {
+				id : isDataGetMail.id,
+				role: isDataGetMail.data,
+				email: isDataGetMail.email,
+				token: isDataGetMail.getJWT()
+			}
+			callback(null, userLogin)
+		}
 	}
 
 	/* watterfall async */
 	Async.waterfall ([
 		getEmail,
-		getDefaultPassword
+		checkValidPassword
 	], (err, response) => {
 		if (!err) {
-			return res.status(200).json({ code: 200, error: false, status: 'success', message: "succes get users", result: response});
+			return res.status(200).json({ code: 200, error: false, status: 'success', message: "succes login", result: response});
 		} else {
-			return res.status(401).json({ code: 401, error: true,status: 'error', message: err, result: null});
+			return res.status(401).json({ code: 401, error: true,status: 'error', message: 'Opps something error', result: null});
 		}
 	})
 }
